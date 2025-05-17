5876bd2c8332fde443bb4a8bb45ddcae23c1a000
Joseph Cox <joeyhobart@outlook.com>
2025-05-17 11:44:11 -0400

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-	base: '/drawit/',
+export default defineConfig(({ mode }) => ({
+	base: mode === 'production' ? '/drawit/' : '/',
 	plugins: [svelte()],
 	build: { outDir: './docs', assetsDir: 'assets' },
-});
+}));
